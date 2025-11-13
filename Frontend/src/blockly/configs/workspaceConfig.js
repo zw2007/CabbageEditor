b600b7de1a0ccd42e3e05c2b018dc41a92d50aee
ALazyDog-oh <488988220@qq.com>
2025-11-13 09:31:03 +0800

    Block美化
    将主题色改成了深色；
    工作区的各个组件都添加了一个相应的颜色；
    将样式代码统一到WorkspaceConfig.js文件，便于以后修改

--- Frontend/src/blockly/configs/workspaceConfig.js
+++ Frontend/src/blockly/configs/workspaceConfig.js
@@ -1,9 +1,60 @@
 import * as Blockly from 'blockly/core';
 import { TOOLBOX_CONFIG } from './toolboxConfig';
 
+export const CoronaTheme = Blockly.Theme.defineTheme('CoronaTheme', {
+  'base': Blockly.Themes.Dark,
+
+  'componentStyles':{
+    'workspaceBackgroundColour':'#1e1e1e',
+    'toolboxBackgroundColour':'#252526',
+    'toolboxForegroundColour':'#ffffff',
+    'flyoutBackgroundColour':'#252526',
+    'flyoutForegroundColour':'#cccccc',
+    'flyoutOpacity':1,
+    'scrollbarColour':'#797979',
+    'insertionMarkerColour':'#ffffff',
+    'insertionMarkerOpacity':0,
+    'scrollbarOpacity':0.4,
+    'cursorColour':'#d0d0d0',
+    'blackoutColour':'rgba(0, 0, 0, .7)',
+  },
+
+  'categoryStyles':{
+    'engine_category':{
+      'colour':'#5631E4'
+    },
+    'appearance_category': {
+      'colour':'#C501F6'
+    },
+    'event_category': {
+      'colour':'#FFDE59'
+    },
+    'control_category': {
+      'colour':'#FFAB19'
+    },
+    'detect_category': {
+      'colour':'#4CBFE6'
+    },
+    'math_category': {
+      'colour':'#59C059'
+    },
+    'variable_category': {
+      'colour':'#FF8C1A'
+    },
+    'list_category': {
+      'colour':'#FF661A'
+    }
+  },
+  'fontStyle':{
+    'family':'"Microsoft YaHei", sans-serif',
+    'weight':'normal',
+    'size':12
+  }
+});
+
 export const WORKSPACE_CONFIG = {
   toolbox: TOOLBOX_CONFIG,
-  theme: Blockly.Themes.Dark,
+  theme: CoronaTheme,
   toolboxPosition: 'start',
   horizontalLayout: false,
   ContextMenu:true,
@@ -11,7 +62,7 @@ export const WORKSPACE_CONFIG = {
     horizontal: false,
     vertical: false,
     set: false
-  },  
+  },
   grid: {
     spacing: 20,
     length: 3,
